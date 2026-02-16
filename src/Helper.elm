4a7c3d6d4419bd1d17796ee64a70e5b65ec37924
up240781-del <up240781@alumnos.upa.edu.mx>
2026-02-15 20:07:36 -0600

    tarea y ejercicios en clase de records

--- src/Helper.elm
+++ src/Helper.elm
@@ -1,1 +1,95 @@
 module Helper exposing (..)
+
+import Html exposing (Html, div, h1, li, text, ul)
+
+
+languages : List { name : String, releaseYear : number, currentVersion : String }
+languages =
+    [ { name = "elm", releaseYear = 2012, currentVersion = "0.19.1" }
+    , { name = "javascript", releaseYear = 1995, currentVersion = "ECMAScript 2025" }
+    ]
+
+
+languageNames list =
+    List.map .name list
+
+
+users : List { name : String, uType : String }
+users =
+    [ { name = "Roberto", uType = "Student" }
+    , { name = "Mitsiu", uType = "Professor" }
+    ]
+
+
+onlyStudents list =
+    let
+        checkStudent user =
+            if user.uType == "Student" then
+                user.name
+
+            else
+                ""
+    in
+    List.map checkStudent list
+
+
+type alias Videogame =
+    { title : String
+    , releaseYear : Int
+    , available : Bool
+    , downloads : Int
+    , genres : List String
+    }
+
+
+games : List Videogame
+games =
+    [ { title = "Minecraft"
+      , releaseYear = 2011
+      , available = True
+      , downloads = 1000
+      , genres = [ "Action", "Shooter" ]
+      }
+    , { title = "Roblox"
+      , releaseYear = 2006
+      , available = True
+      , downloads = 5000
+      , genres = [ "Action", "Adventure" ]
+      }
+    ]
+
+
+getVideogameGenres list =
+    List.map .genres list
+
+
+type alias Computer =
+    { ram : String
+    , model : String
+    , brand : String
+    , screenSize : String
+    }
+
+
+myLaptop : Computer
+myLaptop =
+    { ram = "16GB"
+    , model = "XPS 13"
+    , brand = "Dell"
+    , screenSize = "13.4"
+    }
+
+
+main : Html msg
+main =
+    div []
+        [ h1 [] [ text "My laptop" ]
+        , div []
+            [ ul []
+                [ li [] [ text ("Ram: " ++ myLaptop.ram) ]
+                , li [] [ text ("Modelo: " ++ myLaptop.model) ]
+                , li [] [ text ("Marca: " ++ myLaptop.brand) ]
+                , li [] [ text ("Pulgadas: " ++ myLaptop.screenSize) ]
+                ]
+            ]
+        ]
